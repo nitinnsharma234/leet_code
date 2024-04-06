04e1b1abc3f3035614000d1a9bb498ab0e2438de
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2024-04-06 20:55:31 +0530

    Time: 20 ms (65.71%) | Memory: 15.5 MB (8.64%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-      stack<int> st;
-  for (auto i = 0; i < s.size(); ++i) {
-    if (s[i] == '(') st.push(i);
-    if (s[i] == ')') {
-      if (!st.empty()) st.pop();
-      else s[i] = '*';
-    }
-  }
-  while (!st.empty()) {
-    s[st.top()] = '*';
-    st.pop();
-  }
-  s.erase(remove(s.begin(), s.end(), '*'), s.end());
-  return s;
+        stack <pair<char,int>> st ;
+        string ans;
+        for(int i=0;i<s.length();i++){
+            if(!st.empty() && st.top().first=='(' and s[i]==')'){
+                st.pop();
+            }else if (s[i]=='(' || s[i]==')'){
+                st.push({s[i],i});
+            }
+            ans+=s[i];
+            
+        }
+      
+        while(!st.empty()){
+            int idx=st.top().second;
+
+            st.pop();
+
+            ans.erase(ans.begin()+idx,ans.length()==1?ans.end():ans.begin()+idx+1);
+
+        }
+        return ans;
     }
 };
