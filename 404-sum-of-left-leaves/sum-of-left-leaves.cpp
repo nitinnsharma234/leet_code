7096c7253257f9a12d1e3c194b021ebcc58fbd78
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2024-04-14 20:26:02 +0530

    Time: 4 ms (36.61%) | Memory: 14.8 MB (15.62%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -21,6 +21,27 @@ int leftSum(TreeNode* root , bool isLeft ){
     return leftSum(root->left,true)+leftSum(root->right,false);
 }
     int sumOfLeftLeaves(TreeNode* root) {
-       return  leftSum(root,false);
+        queue<pair<TreeNode*, bool>> qt ; 
+        int sum =0;
+        qt.push({root,false});
+        while(!qt.empty()){
+            pair<TreeNode* , bool> head = qt.front();
+            qt.pop();
+            if (head.first->left){
+                qt.push({head.first->left,true});
+            }
+             if (head.first->right){
+                qt.push({head.first->right,false});
+            }
+            if(!head.first->left and !head.first->right){
+                //i.e it's a leaf node 
+                sum+=head.second?head.first->val:0;
+            }
+
+        }
+        return sum;
+    //    return  leftSum(root,false);
     }
 };
+// space complexity -> 0(H) -> height of the tree
+// time complexity  -> 0(N) -> no. of nodes in the tree
