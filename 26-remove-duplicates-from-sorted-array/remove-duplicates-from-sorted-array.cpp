7be9f165ef06f720a1821c973d73ba0fe2fda434
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2024-05-10 16:22:38 +0530

    Time: 5 ms (78.03%) | Memory: 21 MB (64.36%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -6,12 +6,13 @@ public:
         int curr_el =nums[0];
         int ptr=1;
         while(i<nums.size()){
-            while(i<nums.size() and curr_el ==nums[i]){
-                i++;
-            }
-            if(i<nums.size() and curr_el !=nums[i]){
+           
+            if( curr_el !=nums[i]){
                 curr_el = nums[i];
                 nums[ptr++]=curr_el;
+            }
+            else{
+                i++;
             }
 
         }
