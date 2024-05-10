8c0bd4885a85da400934363c313a91d0ed8416b7
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2024-05-10 13:22:31 +0530

    Time: 194 ms (11.03%) | Memory: 16.9 MB (75.36%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode * mergeTwoLists(ListNode* h1 , ListNode* h2){
+
+    //traversing and comparing nodes of both linked list and setting into a new LL
+    ListNode * root=new ListNode(-1) ;
+    ListNode * temp=root;
+    while(h1 != nullptr and h2 != nullptr){
+       
+        if(h1->val<h2->val){
+            temp ->next =h1;
+            h1=h1->next;
+
+        }
+        else{
+              temp ->next =h2;
+            h2=h2->next;
+        }
+        temp=temp->next;
+    }
+     while(h1!=nullptr){
+            temp->next =h1;
+                h1=h1->next;
+                 temp=temp->next;
+
+        }
+         while(h2!=nullptr){
+            temp->next =h2;
+                h2=h2->next;
+                 temp=temp->next;
+
+        }
+        
+    return root->next;
+
+}
+// ListNode * helper(vector<ListNode*>& lists){
+//     if(n==2){
+//         return mergeTwo
+//     }
+// }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+       ListNode* root =nullptr;
+       for(int i =0;i<lists.size();i++){
+               ListNode * ans= mergeTwoLists(root,lists[i]);
+            root=ans;
+       }
+       return root;
+    }
+};
