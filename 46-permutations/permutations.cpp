3cd888b16aeaa7439c6f108aa2c6be6f2a36211a
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2024-05-13 16:55:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (22.24%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -3,20 +3,20 @@ public:
 void print(vector<int> & nums){
    
 }
-void utils (vector<int> & nums , int idx , vector<int> & perms ,vector<vector<int>> & ans){
-     if (nums.size()==0){
+void utils (vector<int> & nums , int idx  ,vector<vector<int>> & ans){
+     if (idx>=nums.size()){
+        vector<int> perms;
+        for(int x : nums){
+            perms.push_back(x);
+        }
        ans.push_back(perms);
         //return ;
      }
      for (int i =idx; i<nums.size(); i++){
       //  cout<<idx<<" ";
-        int val = nums [i];
-            perms.push_back(val);
-            nums.erase(nums.begin()+i);
-           
-            utils(nums,0, perms,ans);
-            perms.pop_back();
-            nums.insert(nums.begin()+i,val);
+        swap(nums[idx],nums[i]);
+        utils(nums,idx+1,ans);
+          swap(nums[idx],nums[i]);
             
      }
 
@@ -24,7 +24,7 @@ void utils (vector<int> & nums , int idx , vector<int> & perms ,vector<vector<in
     vector<vector<int>> permute(vector<int>& nums) {
             vector<int> perms ; 
             vector<vector<int>> ans ;
-            utils(nums, 0 , perms,ans);
+            utils(nums, 0 ,ans);
             return ans;
     }
 };
