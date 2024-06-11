d2b1f334f62786d32ad8b003c95b8b618c24ab42
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2024-06-11 12:37:26 +0530

    Time: 4 ms (93.76%) | Memory: 22.7 MB (49.47%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -3,19 +3,26 @@ public:
     int trap(vector<int>& height) {
       int n = height.size();
       vector<int> leftSum(n,0);
-      vector<int>rightSum(n,0) ;
+    //   vector<int>rightSum(n,0) ;
       leftSum[0]=height[0];
-      rightSum[n-1]=height[n-1];
+    //   rightSum[n-1]=height[n-1];
       for (int i =1 ;i<n;i++){
         leftSum[i]=max(height[i],leftSum[i-1]);
       }
-      for (int i =n-2 ;i>=0;i--){
-        rightSum[i]=max(height[i],rightSum[i+1]);
-      }
+      int x = height[n-1];
       int trappedWater=0;
-      for(int i =1;i<n-1;i++){
-        trappedWater+=(min(leftSum[i],rightSum[i])-height[i]);
+
+      for (int i =n-2 ;i>=0;i--){
+        if(height[i]>x){
+            x=height[i];
+            continue;
+        }
+        trappedWater+=(min(leftSum[i],x)-height[i]);
+        
       }
+    //   for(int i =1;i<n-1;i++){
+    //     trappedWater+=(min(leftSum[i],rightSum[i])-height[i]);
+    //   }
       return trappedWater;
     }
 };
