532ac1cd514d05438ab0d769550da84f6045d434
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2024-06-17 09:47:21 +0530

    Time: 7 ms (75.56%) | Memory: 12.7 MB (58.61%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-map<char,int> mp={
-    {'I',1,},
-    {'V',5,},
-    {'X',10,},
-    {'L',50,},
-   { 'C',100,},
-   { 'D',500,},
-    {'M',1000}
-};
+  std::map<char, int> mp = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
     int romanToInt(string s) {
-        int num=0;
-        for (int i=0;i<s.size();i++){
-            if (i<s.size() and mp[s[i]]<mp[s[i+1]]){
-                num+=(mp[s[i+1]]-mp[s[i]]);
+        int sum =0;
+        for(int i =0;i<s.size();i++){
+            int no=mp[s[i]];
+            
+            if(i<s.size()-1 and mp[s[i+1]]>mp[s[i]]){
+                no=mp[s[i+1]]-mp[s[i]];
                 i++;
-                continue;
             }
-
-            num+=mp[s[i]];
+            // cout<<no<<" ";
+            sum+=no;
         }
-        return num;
+        return sum;
     }
 };
