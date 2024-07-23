9d2aa030f3e71ec6f72e09594140baed94e19043
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2024-07-23 14:56:13 +0530

    Time: 11 ms (17.92%) | Memory: 14.3 MB (87.86%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+       vector<pair<int,int>>  freq;
+       map<int,int> index;
+       int j=0;
+       for(int x : nums){
+        if (index.find(x)!=index.end()){
+            freq[index[x]].second+=1;
+        }
+        else{
+            index[x]=j++;
+            freq.push_back({x,1});
+        }
+       }
+       sort(freq.begin(),freq.end(),[](pair<int,int> a , pair<int,int> b){
+        if(a.second==b.second){
+            return a.first>b.first;
+        }
+        return b.second>a.second;
+       });
+       int i =0;
+       for(auto x : freq){
+        int f =x.second;
+        cout<<x.first<<" "<<f<<endl;
+
+            while(f--){
+                nums[i++]=x.first;
+            }
+       }
+       return nums;
+    }
+};
+// counting sort 
+// o (n);
