743038467f44924e4ee7b9167fde419c86fde8a8
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2025-05-21 12:06:12 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (95.54%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,55 +1,53 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-       bool  col_zero = false ;
-       int rows=matrix.size();
-       int cols = matrix[0].size();
-       for (int i =0;i<rows;i++)
-       {
-        for (int j=0;j<cols;j++){
-            if(matrix[i][j]==0){
-                 matrix[i][0]=0;
-                 if(j!=0){
-                  matrix[0][j]=0;
-               
-               }
-               else{
-                col_zero=true;
-               }
-               
+        int row = matrix.size(); 
+        int col = matrix[0].size();
+        bool rowZero=false;
+        bool colZero=false;
+        for (int i =0;i<row; i++){
+            for (int j =0;j<col;j++){
+                if (matrix[i][j]==0 ){
+                    if(i==0){
+                      rowZero=true;
+                    }
+                    else if (j==0){
+                        colZero=true;
+                    }
+                   else{
+                     matrix[i][0]=0;
+                    matrix[0][j]=0;
+                   }
+                }
             }
         }
-       }
-     
-       for (int i =1;i<rows;i++){
-        for (int j =1;j<cols;j++){
-            if ( (matrix[0][j]==0|| matrix[i][0]==0)){
+        if(matrix[0][0]==0){colZero=true;}
+        for (int i=1; i<row;i++){
+            for (int j=1;j<col;j++){
+                if (matrix[i][0]==0){
                     matrix[i][j]=0;
+                }
+                if (matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
             }
-            
         }
-       }
-    //      for (int i =0;i<rows;i++){
-    //     for (int j =0;j<cols;j++){
-    //         cout<<matrix[i][j]<<" ";}
-    //         cout<<endl;
-    //    }
-        if(matrix[0][0]==0){
-         for(int i=0;i<cols;i++){
-            matrix[0][i]=0;
+        if(rowZero){
+            for (int i =0;i<col;i++){
+                matrix[0][i]=0;
+            }
         }
-       }
-       if (col_zero){
-        for(int i=0;i<rows;i++){
-            matrix[i][0]=0;
+        if(colZero){
+            for (int i =0;i<row;i++){
+                matrix[i][0]=0;
+            }
         }
-       }
-      
-
+        
     }
+    
 };
 
-// 1 2 3 4 
-// 5 0 7 8 
+// 1 0 3 0 
+// 0 0 7 8 
 // 0 10 11 12
-//13 1 15 0
+// 0 14 15 0 
