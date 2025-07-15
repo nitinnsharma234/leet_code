3b2624e3bfa879fbba9ea72788f19379ac139a81
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2025-07-15 11:54:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (87.14%) - LeetSync

--- 3396-valid-word/valid-word.cpp
+++ 3396-valid-word/valid-word.cpp
@@ -9,15 +9,15 @@ public:
 
         for(char x : word){
              int asciiVal = (int)x;
-            cout<<asciiVal<<endl;
+
             if((asciiVal>=48 && asciiVal<=57)|| (asciiVal>=65 && asciiVal<=90)||(asciiVal>=97 && asciiVal<=122)){
 
                 if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U'){
-                    cout<<"YES"<<x<<endl;
+
                    containsVowel=true; 
                 }
                 else if(asciiVal>57){
-                     cout<<"YES"<<x<<endl;
+
                 containsConsonant=true;
                 }
                 
@@ -26,7 +26,7 @@ public:
                 return false;
             }
         }   
-        cout<<containsConsonant<<" "<<containsVowel<<" ";
+
         return lengthValidity && containsVowel && containsConsonant;
     }
 };
