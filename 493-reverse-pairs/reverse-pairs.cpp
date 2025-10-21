72b4c3ee8be818e63020e51370cf0f40b778765b
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2025-10-21 12:17:06 +0530

    Time: 1221 ms (5.05%) | Memory: 243.4 MB (31.98%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+int     count=0;
+    void sortt(vector<int>& nums,int s ,int e)
+    {   if(s>=e){
+            return ;
+        }
+        int mid =(s+e)/2;
+        sortt(nums,s,mid);
+        sortt(nums,mid+1,e);
+        int j=mid+1;
+           for (int i = s; i <= mid; i++) {
+            while (j <= e && nums[i] > nums[j] * 2LL)
+                j++;
+            count += j - (mid + 1);
+        }
+         j=mid+1;
+        vector<int>vt;
+        int k=0;
+        int i=s;
+      
+        while(i<=mid && j<=e){
+            if(nums[i]<nums[j]){
+                vt.push_back(nums[i]);
+                i++;
+            }
+            else{
+               
+                 vt.push_back(nums[j]);
+                 j++;
+            }
+        }
+        while(i<=mid){
+               vt.push_back(nums[i]);
+                i++;
+        }
+         while(j<=e){
+
+               vt.push_back(nums[j]);
+                j++;
+        }
+
+        for(int i=s;i<=e;i++){
+            nums[i]=vt[k++];
+        }
+    }
+    int reversePairs(vector<int>& nums) {
+        int s=0; 
+        int end = nums.size()-1;
+        sortt(nums,0,end);
+        for(auto x : nums){
+            cout<<x<<" ";
+        }
+        return count;
+    }
+};
