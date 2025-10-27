45fbc3db5c6c78390a3d928c470793808e4fcd16
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2025-10-27 14:28:15 +0530

    Time: 204 ms (10.47%) | Memory: 65.1 MB (5.81%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.ts
@@ -0,0 +1,39 @@
+function splitArray(nums: number[], k: number): number {
+    function canBeSplit(nums:number[],k:number,minSum:number):boolean{
+        let sum:number=0;
+        let partition:number=1;
+        for(const x of nums){
+            if((sum + x) > minSum){
+                sum=0;
+                partition++;
+            }
+            sum+=x;
+            console.log(partition);
+            if(partition>k) return false;
+        }
+        return true;
+    }
+    let low:number=0;
+    let high:number=0;
+    for(const x of nums){
+        low=Math.max(x,low);
+        high+=x;
+    }
+    let ans:number=low;
+    while(low<=high){
+        let mid:number=Math.floor((low+high)/2);
+        console.log("Mid",mid)
+        if(canBeSplit(nums,k,mid)){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
+};
+
+//base observations 
+
+// if k==numes.length return max_element
