2a901ec45f65dbbebe6c45dcee7e58f3ac9c4d16
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2025-10-28 15:19:00 +0530

    Time: 148 ms (18.75%) | Memory: 66.2 MB (6.25%) - LeetSync

---
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.ts
@@ -0,0 +1,34 @@
+function rowAndMaximumOnes(mat: number[][]): number[] {
+    function findIth(row:number[]):number{
+        let low : number =0;
+        let high : number = row.length;
+        row.sort((a,b)=>a-b)
+        let ans:number =-1;
+        while(low<=high){
+            let mid:number= Math.floor((low+high)/2)
+            if(row[mid]==1){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+
+        }
+        console.log(ans);
+        return ans==-1?0:row.length-ans ;
+
+    }
+    let rows: number = mat.length;
+    let ansIdx:number=0;
+    let maxL:number=0;
+    for(let i=0;i<rows;i++){
+        let len :number = findIth(mat[i]);
+        if(len >maxL){
+            ansIdx=i;
+           maxL=len
+        }
+    }
+    return [ansIdx,maxL]
+
+};
