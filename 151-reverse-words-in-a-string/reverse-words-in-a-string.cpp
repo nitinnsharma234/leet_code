75e65d77482bc729a0851265099ae136d8998b6d
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2025-10-29 14:55:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (90.04%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
-    string reversed(string s){
-        int n =s.length();
-         stringstream str(s);
-         string word;
-         int len=0;
-        while (str >> word) {
-                word+=' ';
-                s.insert(n,word);
-                
-                len +=(word.length());
-
-        }
-        return s.substr(n,len-1);
-    }
     string reverseWords(string s) {
-       
-        string st = reversed(s);
-       // cout<<"'"<<st<<"'";
-        return st;
+        // Double reverse, time O(n), space O(1)
+        // - reverse s, remove spaces
+        // - reverse each word
+
+        int i = 0;
+        for (int j = 0; j < s.length(); ++j) {
+            if (s[j] == ' ' && (j == 0 || s[j - 1] == ' ')) continue;
+            s[i] = s[j];
+            ++i;
+        }
+        if (!s.empty() && s.back() == ' ') --i;
+        s.resize(i);
+        if (s.empty()) return s;
+
+        // std::cout << std::format("'{}'", s) << std::endl;
+
+        auto Reverse = [](std::string& s, int left, int right) {
+            while (left < right) {
+                std::swap(s[left], s[right]);
+                ++left;
+                --right;
+            }
+        };
+
+        Reverse(s, 0, s.length() - 1);
+        int word_start = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == ' ') {
+                Reverse(s, word_start, i - 1);
+                word_start = i + 1;
+            }
+        }
+        Reverse(s, word_start, s.length() - 1);
+
+        return s;
     }
 };
