62d612352c1af61169ca1641550d9e794c221ecd
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2025-10-30 12:19:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (91.25%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char,int> freq; 
+        int asciiSymbols[128]{0};
 
-        for ( char x : s){
-            freq[x]++;
+        for (char x : s) {
+            asciiSymbols[(unsigned char)x]++; // use unsigned char to avoid
+                                              // negative indices
         }
-       vector<pair<int,char>> vt;
-        for(auto &[key,val] : freq){
-            vt.push_back({val,key});
+
+        vector<pair<int, char>> vt;
+        for (int i = 0; i < 128; i++) {
+            if (asciiSymbols[i] > 0) {
+                vt.push_back({asciiSymbols[i], (char)i});
+            }
         }
-         sort(vt.begin(), vt.end(), [](pair<int,char>a, pair<int,char>b){
-          return  a.first > b.first;
-         });
-         string ans="";
-         for(auto x : vt){
-            int k=x.first;
-            while(k>0){
-                ans+=x.second;
+        sort(vt.begin(), vt.end(), [](pair<int, char> a, pair<int, char> b) {
+            return a.first > b.first;
+        });
+        string ans = "";
+        for (auto x : vt) {
+            int k = x.first;
+            while (k > 0) {
+                ans += x.second;
                 k--;
             }
-         }
-         return ans;
+        }
+        return ans;
     }
 };
