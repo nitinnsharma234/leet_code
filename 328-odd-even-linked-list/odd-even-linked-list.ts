d68f040ee08135ebb9665816d81422046cb94d77
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2025-11-11 18:00:39 +0530

    Time: 76 ms (5.46%) | Memory: 65.2 MB (6.48%) - LeetSync

---
+++ 328-odd-even-linked-list/odd-even-linked-list.ts
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function oddEvenList(head: ListNode | null): ListNode | null {
+    let oddHead : ListNode | null= null ;
+    let evenHead : ListNode | null= null ;
+    let oddRoot : ListNode | null= new ListNode(-1,null) ;
+    let evenRoot : ListNode | null= new ListNode(-1,null) ;
+    oddHead=oddRoot;
+    evenHead=evenRoot;
+    let root: ListNode | null = head;
+    let i: number =1;
+    while(root!=null){
+
+            if(i%2==0){
+                 console.log(root.val,"--->")
+                evenHead.next =root;
+                evenHead=evenHead.next;
+                // evenHead.next=null;
+            }
+            else{
+                console.log(root.val,">")
+                oddHead.next=root;
+                oddHead=oddHead.next;
+                // oddHead.next=null;
+            }
+
+
+            i++;
+            root=root.next;
+    }
+    evenHead.next=null;
+    oddHead.next=evenRoot.next;
+   
+    return oddRoot.next;
+};
