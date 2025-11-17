3ca979c93b6dcaebd267dfa78bdbf4234614f136
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2025-11-17 08:33:09 +0530

    Time: 1 ms (57.14%) | Memory: 61.2 MB (100.00%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.ts
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.ts
@@ -1,19 +1,13 @@
 function kLengthApart(nums: number[], k: number): boolean {
-    let s : number = -1;
+  let last = -1;
 
-    for(let i =0;i<nums.length;i++){
-        if(nums[i]==1){
-           if(s==-1) {
-            s=i;
-            continue;
-           }
-           else{
-               let  len = i-s-1;
-            //    console.log(i," ",s);
-                if(len<k) return false;
-                s=i;
-           }
-        }
-    }
-    return true ;
-};
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 1) continue;
+
+    if (last !== -1 && i - last - 1 < k) return false;
+
+    last = i;
+  }
+
+  return true;
+}
