c3a3f8f2f54bf06ebb79108122fe535def36123f
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2025-11-17 15:26:14 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (73.64%) - LeetSync

--- 50-powx-n/powx-n.ts
+++ 50-powx-n/powx-n.ts
@@ -1,12 +1,9 @@
 function myPow(x: number, n: number): number {
-    if (n === 0) return 1;
-    if (n < 0) return 1 / myPow(x, -n);
-
-    const half = myPow(x, Math.floor(n / 2));
-
-    if (n % 2 === 0) {
-        return half * half;
-    } else {
-        return half * half * x;
+    if(n==0) return 1 ; 
+    if(n<0) return 1 / myPow(x,-n);
+    const half = myPow(x,Math.floor(n/2));
+    if(n%2==0){
+        return half*half;
     }
-}
+    return x * half * half;
+};
