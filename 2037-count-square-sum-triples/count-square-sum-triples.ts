32a4c916eb4e1341c171682580ce8deac9408650
Nitin Sharma <62827780+nitinnsharma234@users.noreply.github.com>
2025-12-08 12:37:40 +0530

    Time: 9 ms (40.00%) | Memory: 54.4 MB (100.00%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.ts
+++ 2037-count-square-sum-triples/count-square-sum-triples.ts
@@ -1,30 +1,14 @@
 function countTriples(n: number): number {
-    function twoSum(arr :number[],target:number){
-        let ans=0;
-        let i=0;
-        let j = arr.length-1;
-        while(i<j){
-            if(arr[i]+arr[j]==target){
-                ans+=2;
-                i++;
-                j--;
-            }
-            else if (arr[i]+arr[j]>target){
-                j--;
-            }
-            else{
-                i++;
+    let res = 0;
+    // enumerate a and b
+    for (let a = 1; a <= n; a++) {
+        for (let b = 1; b <= n; b++) {
+            // determine if it meets the requirements
+            let c = Math.floor(Math.sqrt(a * a + b * b + 1));
+            if (c <= n && c * c === a * a + b * b) {
+                res++;
             }
         }
-        return ans;
     }
-    const arr :number[]=[];
-    for(let i =1;i<=n;i++){
-        arr.push(i*i);
-    }
-    let ans:number =0;
-    for(let i =n;i>0;i--){
-           ans+= twoSum(arr,i*i);
-    }
-    return ans;
-};
+    return res;
+}
